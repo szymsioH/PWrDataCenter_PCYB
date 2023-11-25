093849aa30dedfe9f1ff2164813ff4e237274085
szymsioH <93218007+szymsioH@users.noreply.github.com>
2023-11-25 22:58:59 +0100

    Kod bruteforc'a, prototyp, działa tylko wewnątrz aplikacji (poprawione).

--- main.cpp
+++ main.cpp
@@ -4,8 +4,24 @@
 using namespace std;
 string crackPassword(const string& pass);
 long long int attempt;
-clock_t start_t;
+clock_t start_t = clock();
 clock_t end_t;
+
+//DICTIONARIES:
+const string dictDigits = "-0123456789";
+const string dictSmallCase = "-abcdefghijklmnopqrstuvwxyz";
+const string dictUppCase = "-ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string dictDigNSmall = "-0123456789abcdefghijklmnopqrstuvwxyz";
+const string dictDigNUpp = "-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string dictSmallNUpp = "-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string dictFull = "-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+string test;
+string alphabet;
+
+int digit[8];
+int alphabetSet = 1;
+int passwordLength = 1;
 
 int main() {
     string password;
@@ -18,25 +34,27 @@ int main() {
     return 0;
 }
 
+void textDisplayFun(int alphabetSet) {
+        switch (alphabetSet) {
+            case 1 : alphabet = dictDigits;
+            cout << "\n\n (Tested password length: " << passwordLength << " )Testing only DIGITS, please wait";  break;
+            case 2 : alphabet = dictSmallCase;
+            cout << "\n\n Couldn't find the password. \n\n Testing LOWERCASE, please wait";  break;
+            case 3 : alphabet = dictUppCase;
+            cout << "\n\n Couldn't find the password. \n\n Testing UPPERCASE, please wait";  break;
+            case 4 : alphabet = dictDigNSmall;
+            cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, LOWERCASE, please wait";  break;
+            case 5 : alphabet = dictDigNUpp;
+            cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, UPPERCASE, please wait";  break;
+            case 6 : alphabet = dictSmallNUpp;
+            cout << "\n\n Couldn't find the password. \n\n Testing LOWERCASE, UPPERCASE, please wait";  break;
+            case 7 : alphabet = dictFull;
+            cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, LOWERCASE, UPPERCASE, please wait";
+            break;
+        }
+}
+
 string crackPassword(const string& pass) {
-
-    //DICTIONARIES:
-    const string dictDigits = "-0123456789";
-    const string dictSmallCase = "-abcdefghijklmnopqrstuvwxyz";
-    const string dictUppCase = "-ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const string dictDigNSmall = "-0123456789abcdefghijklmnopqrstuvwxyz";
-    const string dictDigNUpp = "-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const string dictSmallNUpp = "-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const string dictFull = "-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    string test;
-    string alphabet;
-
-    int digit[8];
-    int alphabetSet = 1;
-    int passwordLength = 1;
-    start_t = clock();
-
     while (true) {
             switch (passwordLength) {
                 case 1:
@@ -78,6 +96,7 @@ string crackPassword(const string& pass) {
                             case 4 : alphabet = dictDigNSmall;
                                      cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, LOWERCASE, please wait";  break;
                             case 5 : alphabet = dictDigNUpp;
+                                     cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, UPPERCASE, please wait";  break;
                         }
                         for (digit[1] = 0; digit[1] < alphabet.length(); digit[1]++)
                             for (digit[0] = 1; digit[0] < alphabet.length(); digit[0]++) {
@@ -99,22 +118,8 @@ string crackPassword(const string& pass) {
                 case 3:
                     alphabetSet = 1;
                     while (alphabetSet < 8) {
-                        switch (alphabetSet) {
-                            case 1 : alphabet = dictDigits;
-                                     cout << "\n\n (Tested password length: " << passwordLength << " )Testing only DIGITS, please wait";  break;
-                            case 2 : alphabet = dictSmallCase;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing LOWERCASE, please wait";  break;
-                            case 3 : alphabet = dictUppCase;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing UPPERCASE, please wait";  break;
-                            case 4 : alphabet = dictDigNSmall;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, LOWERCASE, please wait";  break;
-                            case 5 : alphabet = dictDigNUpp;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, UPPERCASE, please wait";  break;
-                            case 6 : alphabet = dictSmallNUpp;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing LOWERCASE, UPPERCASE, please wait";  break;
-                            case 7 : alphabet = dictFull;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, LOWERCASE, UPPERCASE, please wait";  break;
-                        }
+                        textDisplayFun(alphabetSet);
+                        alphabetSet++;
                         for (digit[2] = 0; digit[2] < alphabet.length(); digit[2]++)
                             for (digit[1] = 0; digit[1] < alphabet.length(); digit[1]++)
                                 for (digit[0] = 1; digit[0] < alphabet.length(); digit[0]++) {
@@ -130,28 +135,12 @@ string crackPassword(const string& pass) {
                                         return test;
                                     }
                                 }
-                        alphabetSet++;
                     }
                     break;
                 case 4:
                     alphabetSet = 1;
                     while (alphabetSet < 8){
-                        switch (alphabetSet) {
-                            case 1 : alphabet = dictDigits;
-                                     cout << "\n\n (Tested password length: " << passwordLength << " )Testing only DIGITS, please wait";  break;
-                            case 2 : alphabet = dictSmallCase;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing LOWERCASE, please wait";  break;
-                            case 3 : alphabet = dictUppCase;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing UPPERCASE, please wait";  break;
-                            case 4 : alphabet = dictDigNSmall;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, LOWERCASE, please wait";  break;
-                            case 5 : alphabet = dictDigNUpp;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, UPPERCASE, please wait";  break;
-                            case 6 : alphabet = dictSmallNUpp;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing LOWERCASE, UPPERCASE, please wait";  break;
-                            case 7 : alphabet = dictFull;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, LOWERCASE, UPPERCASE, please wait";  break;
-                        }
+                        textDisplayFun(alphabetSet);
                         for (digit[3] = 0; digit[3] < alphabet.length(); digit[3]++)
                             for (digit[2] = 0; digit[2] < alphabet.length(); digit[2]++)
                                 for (digit[1] = 0; digit[1] < alphabet.length(); digit[1]++)
@@ -173,22 +162,7 @@ string crackPassword(const string& pass) {
                 case 5:
                     alphabetSet = 1;
                     while (alphabetSet < 8) {
-                        switch (alphabetSet) {
-                            case 1 : alphabet = dictDigits;
-                                     cout << "\n\n (Tested password length: " << passwordLength << " )Testing only DIGITS, please wait";  break;
-                            case 2 : alphabet = dictSmallCase;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing LOWERCASE, please wait";  break;
-                            case 3 : alphabet = dictUppCase;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing UPPERCASE, please wait";  break;
-                            case 4 : alphabet = dictDigNSmall;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, LOWERCASE, please wait";  break;
-                            case 5 : alphabet = dictDigNUpp;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, UPPERCASE, please wait";  break;
-                            case 6 : alphabet = dictSmallNUpp;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing LOWERCASE, UPPERCASE, please wait";  break;
-                            case 7 : alphabet = dictFull;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, LOWERCASE, UPPERCASE, please wait";  break;
-                        }
+                        textDisplayFun(alphabetSet);
                         for (digit[4] = 0; digit[4] < alphabet.length(); digit[4]++)
                             for (digit[3] = 0; digit[3] < alphabet.length(); digit[3]++)
                                 for (digit[2] = 0; digit[2] < alphabet.length(); digit[2]++)
@@ -212,22 +186,7 @@ string crackPassword(const string& pass) {
                 case 6:
                     alphabetSet = 1;
                     while (alphabetSet < 8) {
-                        switch (alphabetSet) {
-                            case 1 : alphabet = dictDigits;
-                                     cout << "\n\n (Tested password length: " << passwordLength << " )Testing only DIGITS, please wait";  break;
-                            case 2 : alphabet = dictSmallCase;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing LOWERCASE, please wait";  break;
-                            case 3 : alphabet = dictUppCase;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing UPPERCASE, please wait";  break;
-                            case 4 : alphabet = dictDigNSmall;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, LOWERCASE, please wait";  break;
-                            case 5 : alphabet = dictDigNUpp;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, UPPERCASE, please wait";  break;
-                            case 6 : alphabet = dictSmallNUpp;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing LOWERCASE, UPPERCASE, please wait";  break;
-                            case 7 : alphabet = dictFull;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, LOWERCASE, UPPERCASE, please wait";  break;
-                        }
+                        textDisplayFun(alphabetSet);
                             for (digit[5] = 0; digit[5] < alphabet.length(); digit[5]++)
                                 for (digit[4] = 0; digit[4] < alphabet.length(); digit[4]++)
                                     for (digit[3] = 0; digit[3] < alphabet.length(); digit[3]++)
@@ -252,22 +211,7 @@ string crackPassword(const string& pass) {
                 case 7:
                     alphabetSet = 1;
                     while (alphabetSet < 8) {
-                        switch (alphabetSet) {
-                            case 1 : alphabet = dictDigits;
-                                     cout << "\n\n (Tested password length: " << passwordLength << " )Testing only DIGITS, please wait";  break;
-                            case 2 : alphabet = dictSmallCase;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing LOWERCASE, please wait";  break;
-                            case 3 : alphabet = dictUppCase;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing UPPERCASE, please wait";  break;
-                            case 4 : alphabet = dictDigNSmall;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, LOWERCASE, please wait";  break;
-                            case 5 : alphabet = dictDigNUpp;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, UPPERCASE, please wait";  break;
-                            case 6 : alphabet = dictSmallNUpp;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing LOWERCASE, UPPERCASE, please wait";  break;
-                            case 7 : alphabet = dictFull;
-                                     cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, LOWERCASE, UPPERCASE, please wait";  break;
-                        }
+                        textDisplayFun(alphabetSet);
                         for (digit[6] = 0; digit[6] < alphabet.length(); digit[6]++)
                             for (digit[5] = 0; digit[5] < alphabet.length(); digit[5]++)
                                 for (digit[4] = 0; digit[4] < alphabet.length(); digit[4]++)
@@ -293,22 +237,7 @@ string crackPassword(const string& pass) {
                 case 8:
                     alphabetSet = 1;
                     while (alphabetSet < 8) {
-                        switch (alphabetSet) {
-                            case 1 : alphabet = dictDigits;
-                                cout << "\n\n (Tested password length: " << passwordLength << " )Testing only DIGITS, please wait";  break;
-                            case 2 : alphabet = dictSmallCase;
-                                cout << "\n\n Couldn't find the password. \n\n Testing LOWERCASE, please wait";  break;
-                            case 3 : alphabet = dictUppCase;
-                                cout << "\n\n Couldn't find the password. \n\n Testing UPPERCASE, please wait";  break;
-                            case 4 : alphabet = dictDigNSmall;
-                                cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, LOWERCASE, please wait";  break;
-                            case 5 : alphabet = dictDigNUpp;
-                                cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, UPPERCASE, please wait";  break;
-                            case 6 : alphabet = dictSmallNUpp;
-                                cout << "\n\n Couldn't find the password. \n\n Testing LOWERCASE, UPPERCASE, please wait";  break;
-                            case 7 : alphabet = dictFull;
-                                cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, LOWERCASE, UPPERCASE, please wait";  break;
-                        }
+                        textDisplayFun(alphabetSet);
                         for (digit[7] = 0; digit[7] < alphabet.length(); digit[7]++)
                             for (digit[6] = 0; digit[6] < alphabet.length(); digit[6]++)
                                 for (digit[5] = 0; digit[5] < alphabet.length(); digit[5]++)
@@ -335,22 +264,7 @@ string crackPassword(const string& pass) {
                 case 9:
                     alphabetSet = 1;
                     while (alphabetSet < 8) {
-                        switch (alphabetSet) {
-                            case 1 : alphabet = dictDigits;
-                                cout << "\n\n (Tested password length: " << passwordLength << " )Testing only DIGITS, please wait";  break;
-                            case 2 : alphabet = dictSmallCase;
-                                cout << "\n\n Couldn't find the password. \n\n Testing LOWERCASE, please wait";  break;
-                            case 3 : alphabet = dictUppCase;
-                                cout << "\n\n Couldn't find the password. \n\n Testing UPPERCASE, please wait";  break;
-                            case 4 : alphabet = dictDigNSmall;
-                                cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, LOWERCASE, please wait";  break;
-                            case 5 : alphabet = dictDigNUpp;
-                                cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, UPPERCASE, please wait";  break;
-                            case 6 : alphabet = dictSmallNUpp;
-                                cout << "\n\n Couldn't find the password. \n\n Testing LOWERCASE, UPPERCASE, please wait";  break;
-                            case 7 : alphabet = dictFull;
-                                cout << "\n\n Couldn't find the password. \n\n Testing DIGITS, LOWERCASE, UPPERCASE, please wait";  break;
-                        }
+                        textDisplayFun(alphabetSet);
                         for (digit[8] = 0; digit[8] < alphabet.length(); digit[8]++)
                             for (digit[7] = 0; digit[7] < alphabet.length(); digit[7]++)
                                 for (digit[6] = 0; digit[6] < alphabet.length(); digit[6]++)
